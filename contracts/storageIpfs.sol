d637d71d4936a8ba421194b4da1a89c88df409a2
Elkadiri Zouhra <117985245+elkadirizouhra@users.noreply.github.com>
2025-01-20 13:25:33 +0100

    Update storageIpfs.sol

--- contracts/storageIpfs.sol
+++ contracts/storageIpfs.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 contract fileStorage {
     struct File {
         string cid;
@@ -8,26 +7,20 @@ contract fileStorage {
         string fileType;
         uint256 fileSize;
     }
-
     mapping(address => File[]) public userFiles;
-
     event FileUploaded(address indexed user, string cid, string fileName, string fileType, uint256 fileSize);
-
     function uploadFile(string memory _cid, string memory _fileName, string memory _fileType, uint256 _fileSize) public {
         require(bytes(_cid).length > 0, "CID cannot be empty");
         require(bytes(_fileName).length > 0, "File name cannot be empty");
         require(bytes(_fileType).length > 0, "File type cannot be empty");
         require(_fileSize > 0, "File size must be greater than 0");
-
         File memory newFile = File(_cid, _fileName, _fileType, _fileSize);
         userFiles[msg.sender].push(newFile);
         emit FileUploaded(msg.sender, _cid, _fileName, _fileType, _fileSize);
     }
-
     function getFiles(address _user) public view returns (File[] memory) {
         return userFiles[_user];
     }
-
     function getFileByName(address _user, string memory _fileName) public view returns (string memory ) {
         require(bytes(_fileName).length > 0, "File name cannot be empty");
         File[] memory files = userFiles[_user];
